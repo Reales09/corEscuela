9097056d44ce7d1e789bdffe2b43b8b8e57d48b5
Reales Myles <rclarence123@gmail.com>
2023-04-25 12:01:35 -0500

    Primer commit corescuela

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 2023);  
+            escuela.Pais ="Colombia";
+            escuela.Ciudad = "Bogotá";
+            Console.WriteLine(escuela.Nombre);
+           
+        }
+    }
+    
+}
+
+    
