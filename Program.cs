4291e86dde934148bec7e4bba47420e793c3944d
Reales Myles <rclarence123@gmail.com>
2023-04-26 08:43:43 -0500

    Se agrega cursos y tipos de jornadas

--- Program.cs
+++ Program.cs
@@ -7,10 +7,32 @@ namespace Etapa1
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Platzi Academy", 2023);  
-            escuela.Pais ="Colombia";
-            escuela.Ciudad = "Bogotá";
-            Console.WriteLine(escuela.Nombre);
+            var escuela = new Escuela("Platzi Academy", 2023,
+             TiposDeEscuela.Primaria,             
+              ciudad: "Bogotá"
+             );  
+
+             var curso1 = new Curso(){
+                 Nombre = "101", 
+             };
+
+              var curso2 = new Curso(){
+                 Nombre = "201", 
+             };
+
+                var curso3 = new Curso(){
+                 Nombre = "301", 
+             };
+            
+            Console.WriteLine(escuela);
+
+            System.Console.WriteLine("==============");
+
+            Console.WriteLine(curso1.Nombre + ", " + curso1.UniqueId);
+
+            Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
+
+            Console.WriteLine(curso3);
            
         }
     }
---
+++ entidades/TiposDeEscuela.cs
@@ -0,0 +1,10 @@
+namespace CoreEscuela.Entidades
+{
+    public enum TiposDeEscuela
+    {
+        Primaria, 
+        Secundaria, 
+        PreEscolar
+        
+    }
+}
